02cccdfd372c2506c0dff64833223dba0d4262f1
AndrewMowe <andrew@team-eight.com>
2015-11-17 20:45:51 -0600

    style search form

--- 404.php
+++ 404.php
@@ -7,8 +7,8 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<div id="primary" class="content-area container">
+		<main id="main" class="site-main blog-main" role="main">
 
 			<section class="error-404 not-found">
 				<header class="page-header">
@@ -20,37 +20,13 @@ get_header(); ?>
 
 					<?php get_search_form(); ?>
 
-					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
-
-					<?php if ( epic_scan_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'epic-scan' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-					<?php endif; ?>
-
-					<?php
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'epic-scan' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-					?>
-
-					<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
-
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
 		</main><!-- #main -->
+
+		<?php get_sidebar(); ?>
+		
 	</div><!-- #primary -->
 
 <?php get_footer(); ?>
--- js/script.js
+++ js/script.js
@@ -2,20 +2,28 @@
   "use strict";
 
   $(document).ready(function() {
+
+  	// vars for home hero bg video
 	  var $target = $("#target"),
 	  		video = $("#video-es").html();
+
+		if( $target.length > 0 ) {
+			bgVideoSet($target, video);
+		}
+
+	});
+
+
+	// detect mobile and display video if not
+	function bgVideoSet(target, video) {
 
 	  if( $.browser.mobile ) {
 	  	var isMobile = true;
 	  } else {
 	  	var isMobile = false;
-	  	$target.html(video);
+	  	target.html(video);
 	  }
 
-	  console.log(isMobile);
-
-	});
-
-
+	}
 
 }( window.jQuery, window, document ));
